b3f6a304ff08d8a2399f6efd2d5a75e4375bd2a7
Eamonn de Leastar <edeleastar@wit.ie>
2020-10-20 13:23:06 +0100

    introduce header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<div class="uk-flex uk-flex-center uk-flex-middle uk-text-center uk-padding-small" uk-grid>
+  <div class="uk-child-width-1-2@s uk-card uk-card-default uk-grid-collapse" uk-grid>
+    <div>
+      <div class="uk-tile uk-tile-default uk-padding-small">
+        <h2>Oileain</h2>
+      </div>
+    </div>
+    <div>
+      <div class="uk-tile uk-tile-muted uk-padding-small">
+        <div class="uk-text-small">
+          An experiment in exploring
+          <a href="http://www.oileain.org/">David Walsh's Oileain</a>. Prepared by Eamonn de Leastar (edeleastar@gmail.com)
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
