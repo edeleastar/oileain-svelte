413a7ce093147b5f2b8a63e1f3f2e49066ce3a7a
Eamonn de Leastar <edeleastar@wit.ie>
2020-10-20 13:42:18 +0100

    move head elements into app

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,12 @@
   import Header from "./components/Header.svelte";
 </script>
 
-<Header />
+<svelte:head>
+  <title>Oileain</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.8/dist/css/uikit.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.8/dist/js/uikit.min.js">
+  </script>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css" />
+</svelte:head>
+
+<Header title="Oileain" />
