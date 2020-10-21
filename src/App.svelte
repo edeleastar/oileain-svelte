f1f59ac068632656cb1dbe39892ea68787d6b9a8
Eamonn de Leastar <edeleastar@wit.ie>
2020-10-21 08:28:46 +0100

    populate map from api

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { CoastalLeafletMap } from "./services/coastal-leaflet";
+
   import Header from "./components/Header.svelte";
+
+  let coasts: any[];
+
+  const mapDescriptor = {
+    id: "home-map-id",
+    height: 800,
+    location: { lat: 53.2734, long: -7.7783203 },
+    zoom: 8,
+    minZoom: 7,
+    activeLayer: "",
+  };
+
+  let map: CoastalLeafletMap;
+
+  onMount(async () => {
+    const response = await fetch("https://edeleastar.github.io/oileain-api/all-slim.json");
+    coasts = await response.json();
+    console.log(coasts);
+    map = new CoastalLeafletMap(mapDescriptor);
+    map.populateCoasts(coasts);
+  });
 </script>
 
 <svelte:head>
@@ -11,3 +35,6 @@
 </svelte:head>
 
 <Header title="Oileain" />
+<div class="uk-container">
+  <div id={mapDescriptor.id} style="height:{mapDescriptor.height}px" />
+</div>
